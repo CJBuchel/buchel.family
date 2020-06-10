a2aff23ce12547f6b7e4c6e563c964389486cc49
CJBuchel <connorbuchel@gmail.com>
2020-06-10 16:31:19 +0000

    Added hostname configs and script files for ease of use

---
+++ bootstrap.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Sets up ruby on rails project
+
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.7.1
+rbenv global 2.7.1
+ruby -v
+
+gem install bundler
+
+gem install rails -v 6.0.2.2
+
+rbenv rehash
+
+rails -v
+
+sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+
+mkdir buchel.family
+cd buchel.family/
+
+git init
+
+git remote add origin https://github.com/CJBuchel/buchel.family.git
+
+git pull origin master
+
+sudo chmod +x start
+sudo chmod +x stop
+sudo chmod +x restart
+
+./start #Starts server
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # host names allowed
+  config.hosts << "buchel.family"
+  config.hosts << "blitzen.duckdns.org"
+  config.hosts << "blitzen-alpha.com"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
