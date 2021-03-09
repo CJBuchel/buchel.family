c632508cb999552fd4c3e94008ef22df13aa219a
CJBuchel <connorbuchel@gmail.com>
2021-03-09 16:15:46 +0800

    Fixing Passenger install

--- nginxConfig.sh
+++ nginxConfig.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
 # Install Nginx & Passenger
-echo "Installing nginx & Passenger"
+# echo "Installing nginx & Passenger"
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+# sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+# sudo apt-get update
+# sudo apt-get install -y nginx-extras libnginx-mod-http-passenger
+# if [ ! -f /etc/nginx/modules-enabled/50-mod-http-passenger.conf ]; then sudo ln -s /usr/share/nginx/modules-available/mod-http-passenger.load /etc/nginx/modules-enabled/50-mod-http-passenger.conf ; fi
+# sudo ls /etc/nginx/conf.d/mod-http-passenger.conf
+
+# Install our PGP key and add HTTPS support for APT
+sudo apt-get install -y dirmngr gnupg
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get install -y apt-transport-https ca-certificates
+
+# Add our APT repository
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger focal main > /etc/apt/sources.list.d/passenger.list'
 sudo apt-get update
-sudo apt-get install -y nginx-extras libnginx-mod-http-passenger
-if [ ! -f /etc/nginx/modules-enabled/50-mod-http-passenger.conf ]; then sudo ln -s /usr/share/nginx/modules-available/mod-http-passenger.load /etc/nginx/modules-enabled/50-mod-http-passenger.conf ; fi
+
+# Install Passenger + Nginx module
+sudo apt-get install -y libnginx-mod-http-passenger
+
+echo "Passenger Location: "
+echo " "
 sudo ls /etc/nginx/conf.d/mod-http-passenger.conf
 
 # Replace line in passenger.conf to point to ruby
