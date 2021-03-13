7e71cfd556d2a85742437218cc2f1138bd81d295
CJBuchel <connorbuchel@gmail.com>
2021-03-13 16:50:25 +0800

    Fixed rootLocations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+sudo mkdir /var/www
+sudo chmod +x /var/www/*
+
+
 # Install script for a server (Installs everything needed to run site on server)
-git remote add latest https://github.com/CJBuchel/buchel.family.git
-git pull latest master
+sudo apt-get install git
+sudo git clone https://github.com/CJBuchel/buchel.family.git /var/www/
+sudo cd /var/www/buchel.family
+
+sudo git remote add latest https://github.com/CJBuchel/buchel.family.git
+sudo git pull latest master
 
 sudo chmod +x install_server.sh
 sudo ./install_server.sh
--- nginxConfig.sh
+++ nginxConfig.sh
@@ -12,6 +12,8 @@
 # Install our PGP key and add HTTPS support for APT
 # Install Nginx & Passenger
 # echo "Installing nginx & Passenger"
+read -p "Username for server e.g [cj]: " user
+
 sudo apt-get install -y nginx
 sudo apt-get install -y dirmngr gnupg
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
@@ -24,6 +26,7 @@ sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger foc
 sudo apt-get update
 
 # Install Passenger + Nginx module
+sudo apt-get install -y nginx-extras passenger
 sudo apt-get install -y libnginx-mod-http-passenger
 
 if [ ! -f /etc/nginx/modules-enabled/50-mod-http-passenger.conf ]; then sudo ln -s /usr/share/nginx/modules-available/mod-http-passenger.load /etc/nginx/modules-enabled/50-mod-http-passenger.conf ; fi
@@ -64,7 +67,8 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo cp nginxTemplate /etc/nginx/sites-enabled/buchel.family
 
 # logjam prevention
-( cd ~/; openssl dhparam -out dhparams.pem 2048 )
+sudo mkdir /var/www/dhparams/
+( cd /var/www/dhparams/; openssl dhparam -out dhparams.pem 2048 )
 
 sudo nginx -t
 sudo systemctl reload nginx
