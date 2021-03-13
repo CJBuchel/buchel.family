f3bbf63f53d49556851ec2e658a8ed471c7528b3
CJBuchel <connorbuchel@gmail.com>
2021-03-13 12:53:38 +0800

    Added precompiler and install to ./start script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "starting rails server"
+
+echo "Yarn installer"
+yarn install
+
+echo "Installing gems"
+bundle install
+
+echo "Compiling assets for production"
+rake assets:precompile
+
+
+sudo service nginx start
