fa09b1ec1a7468a92fbb11de57539f89b7a0e9e4
CJBuchel <connorbuchel@gmail.com>
2021-03-14 19:46:25 +0800

    Fixed var/www

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo chmod +x /var/www/*
 
 # Install script for a server (Installs everything needed to run site on server)
 sudo apt-get install git
-sudo git clone https://github.com/CJBuchel/buchel.family.git /var/www/
+sudo git clone https://github.com/CJBuchel/buchel.family.git /var/www/buchel.family
 sudo cd /var/www/buchel.family
 
 sudo git remote add latest https://github.com/CJBuchel/buchel.family.git
